6371367eed5d8b0e710a477c7812a02bdc7b0f55
Adhitya Mohan <me@adhityamohan.in>
2020-10-08 20:10:11 +0530

    oneplus7t: override with redfin R fp
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- init/init_oneplus7t.cpp
+++ init/init_oneplus7t.cpp
@@ -49,5 +49,13 @@ void property_override(char const prop[], char const value[]) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_multi(char const system_prop[], char const vendor_prop[],char const bootimage_prop[], char const value[]) {
+    property_override(odm_prop, value);
+    property_override(vendor_prop, value);
+    property_override(bootimage_prop, value);
+}
+
 void vendor_load_properties() {
+  // fingerprint
+  property_override_multi("ro.odm.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys");
 }
