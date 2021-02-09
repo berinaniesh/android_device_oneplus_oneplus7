b64bef3777d3a01b32d716fc2860c7cce0bd9b4e
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-02-09 21:31:58 +0530

    oneplus7t: rootdir: fix location of scripts
    * take a shot
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -14,7 +14,7 @@ LOCAL_MODULE       := init.oneplus.haptics.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.oneplus.haptics.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -22,7 +22,7 @@ LOCAL_MODULE       := init.oneplus.overlay.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.oneplus.overlay.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
